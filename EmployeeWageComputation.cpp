dbd1c0d6ed57e75848f5b81de7a271c99ecffcf9
Srijan-18 <srijan.mishrasir@gmail.com>
2020-08-26 06:36:44 +0530

    ADDED: Generation and display of attendance for an employee.

--- EmployeeWageComputation.cpp
+++ EmployeeWageComputation.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
-
+#include <ctime>
 using namespace std;
+
+void printAttendance();
 
 int main()
 {
     cout << "WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM." << endl;
+    printAttendance();
+}
+
+void printAttendance()
+{
+    srand(time(0));
+
+    const int PRESENT = 1;
+
+    int attendance = rand() % 2;
+    attendance == PRESENT ? cout << "EMPLOYEE IS PRESENT" << endl: cout << "EMPLOYEE IS ABSENT" << endl;
 }
