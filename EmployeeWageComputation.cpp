66fb6636f766c857d57c6a7b65da60ea1a2ba185
Srijan-18 <srijan.mishrasir@gmail.com>
2020-08-26 07:11:16 +0530

    ADDED: Generation of daily wage of an employee.

--- EmployeeWageComputation.cpp
+++ EmployeeWageComputation.cpp
@@ -2,20 +2,51 @@
 #include <ctime>
 using namespace std;
 
-void printAttendance();
+int getAttendance();
+void generateDailyWage();
+int getWageByAttendanceAndRate(int, int);
+int getWorkingHours(int);
 
 int main()
 {
     cout << "WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM." << endl;
-    printAttendance();
+    generateDailyWage();
 }
 
-void printAttendance()
+void generateDailyWage()
+{
+    const int WAGE_RATE = 20;
+    int dailyWage = 0;
+    int attendance = getAttendance();
+
+    dailyWage = getWageByAttendanceAndRate(attendance, WAGE_RATE);
+    cout << "DAILY WAGE OF EMPLOYEE IS: " << dailyWage << endl;
+}
+
+int getAttendance()
 {
     srand(time(0));
 
-    const int PRESENT = 1;
+    return rand() % 2;
+}
 
-    int attendance = rand() % 2;
-    attendance == PRESENT ? cout << "EMPLOYEE IS PRESENT" << endl: cout << "EMPLOYEE IS ABSENT" << endl;
+int getWageByAttendanceAndRate(int attendance, int rate)
+{
+    int workingHours = getWorkingHours(attendance);
+    return workingHours * rate;
+}
+
+int getWorkingHours(int attendance)
+{
+    const int PRESENT = 1;
+    const int FULL_DAY_HOURS = 8;
+    const int ABSENT_HOURS = 0;
+
+    if (attendance == PRESENT)
+    {
+        cout << "EMPLOYEE IS PRESENT AND ";
+        return FULL_DAY_HOURS;
+    }
+    cout << "EMPLOYEE IS ABSENT AND ";
+    return ABSENT_HOURS;
 }
