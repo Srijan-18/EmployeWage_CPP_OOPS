f749e5a72979b9a961c3f60e1f484a4f4b958c7f
Srijan-18 <srijan.mishrasir@gmail.com>
2020-08-26 08:25:16 +0530

    REFACTORED: Optimisation of code by removal of unwanted functions.

--- EmployeeWageComputation.cpp
+++ EmployeeWageComputation.cpp
@@ -2,15 +2,15 @@
 #include <ctime>
 using namespace std;
 
-int getDailyWage();
-int getWageByAttendanceAndRate(int, int);
-inline int getWorkingHours(int);
+int getDailyWage(int);
+inline int getAttendance();
+int getDailyWorkingHours();
 void generateMonthlyWage(int);
 
 int main()
 {
     cout << "WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM." << endl;
-    
+
     const int MAX_WORKING_DAYS = 20;
     generateMonthlyWage(MAX_WORKING_DAYS);
 
@@ -22,40 +22,36 @@ void generateMonthlyWage(int MAX_WORKING_DAYS)
     srand(time(0)); //setting seed for random function in order to get different random values for attendance.
     int dayCount = 0, monthlyWage = 0;
 
-    while (dayCount < 20)
+    while (dayCount < MAX_WORKING_DAYS)
     {
-        monthlyWage += getDailyWage();
+        int dailyHours = getDailyWorkingHours();
+        monthlyWage += getDailyWage(dailyHours);
         dayCount++;
     }
 
     cout << "MONTHLY WAGE OF EMPLOYEE IS: " << monthlyWage << endl;
 }
 
-int getDailyWage()
+int getDailyWage(int hours)
 {
     const int WAGE_RATE = 20;
     int dailyWage = 0;
-    int attendance = rand() % 3;
 
-    dailyWage = getWageByAttendanceAndRate(attendance, WAGE_RATE);
+    dailyWage = hours * WAGE_RATE;
     cout << "DAILY WAGE OF EMPLOYEE IS: " << dailyWage << endl;
 
     return dailyWage;
 }
 
-int getWageByAttendanceAndRate(int attendance, int rate)
-{
-    int workingHours = getWorkingHours(attendance);
-    return workingHours * rate;
-}
-
-inline int getWorkingHours(int attendance)
+inline int getDailyWorkingHours()
 {
     const int FULL_TIME = 1;
     const int PART_TIME = 2;
     const int FULL_DAY_HOURS = 8;
     const int PART_TIME_HOURS = 4;
     const int ABSENT_HOURS = 0;
+
+    int attendance = getAttendance();
 
     switch (attendance)
     {
@@ -71,4 +67,9 @@ inline int getWorkingHours(int attendance)
         cout << "EMPLOYEE IS ABSENT AND ";
         return ABSENT_HOURS;
     }
+}
+
+inline int getAttendance()
+{
+    return rand() % 3;
 }
