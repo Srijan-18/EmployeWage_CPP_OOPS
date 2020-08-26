6345e78ba92c666f2788b9ec4dd8001bc99359c9
Srijan-18 <srijan.mishrasir@gmail.com>
2020-08-26 07:22:11 +0530

    ADDED: Generation of wage for part time employee too.

--- EmployeeWageComputation.cpp
+++ EmployeeWageComputation.cpp
@@ -27,7 +27,7 @@ int getAttendance()
 {
     srand(time(0));
 
-    return rand() % 2;
+    return rand() % 3;
 }
 
 int getWageByAttendanceAndRate(int attendance, int rate)
@@ -38,15 +38,23 @@ int getWageByAttendanceAndRate(int attendance, int rate)
 
 int getWorkingHours(int attendance)
 {
-    const int PRESENT = 1;
+    const int FULL_TIME = 1;
+    const int PART_TIME = 2;
     const int FULL_DAY_HOURS = 8;
+    const int PART_TIME_HOURS = 4;
     const int ABSENT_HOURS = 0;
 
-    if (attendance == PRESENT)
+    if (attendance == FULL_TIME)
     {
-        cout << "EMPLOYEE IS PRESENT AND ";
+        cout << "EMPLOYEE IS PRESENT FULL TIME AND ";
         return FULL_DAY_HOURS;
     }
+    else if (attendance == PART_TIME)
+    {
+        cout << "EMPLOYEE IS PRESENT PART TIME AND ";
+        return PART_TIME_HOURS;
+    }
+
     cout << "EMPLOYEE IS ABSENT AND ";
     return ABSENT_HOURS;
 }
