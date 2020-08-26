0119e49072f21eb557acd571fa144ce146885d55
Srijan-18 <srijan.mishrasir@gmail.com>
2020-08-26 09:05:13 +0530

    REFACTORED: Use of Class and Class Variables.

--- EmployeeWageComputation.cpp
+++ EmployeeWageComputation.cpp
@@ -2,76 +2,78 @@
 #include <ctime>
 using namespace std;
 
-int getDailyWage(int);
-inline int getAttendance();
-int getDailyWorkingHours();
-void generateMonthlyWage(int, int);
-
-int main()
+class EmployeeWage
 {
-    cout << "WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM." << endl;
-
     const int MAX_WORKING_DAYS = 20;
     const int MAX_WORKING_HOURS = 100;
-    generateMonthlyWage(MAX_WORKING_DAYS, MAX_WORKING_HOURS);
+    const int WAGE_RATE = 20;
+    int dailyWage = 0, dailyHours = 0;
+    int dayCount = 0, monthlyWage = 0, hoursCount = 0;
 
-    return 0;
-}
+    inline int getAttendance()
+    {
+        return rand() % 3;
+    }
 
-void generateMonthlyWage(int MAX_WORKING_DAYS, int MAX_WORKING_HOURS)
+    inline int generatedDailyWorkingHours()
+    {
+        const int FULL_TIME = 1;
+        const int PART_TIME = 2;
+        const int FULL_DAY_HOURS = 8;
+        const int PART_TIME_HOURS = 4;
+        const int ABSENT_HOURS = 0;
+
+        int attendance = getAttendance();
+
+        switch (attendance)
+        {
+        case PART_TIME:
+            cout << "EMPLOYEE IS PRESENT PART TIME AND ";
+            dailyHours = PART_TIME_HOURS;
+            break;
+        case FULL_TIME:
+            cout << "EMPLOYEE IS PRESENT FULL TIME AND ";
+            dailyHours = FULL_DAY_HOURS;
+            break;
+        default:
+            cout << "EMPLOYEE IS ABSENT AND ";
+            dailyHours = ABSENT_HOURS;
+        }
+        
+        return dailyHours;
+    }
+
+    int getDailyWage()
+    {
+        dailyWage = dailyHours * WAGE_RATE;
+        cout << "DAILY WAGE OF EMPLOYEE IS: " << dailyWage << endl;
+
+        return dailyWage;
+    }
+
+public:
+    void generateMonthlyWage();
+};
+
+void EmployeeWage::generateMonthlyWage()
 {
     srand(time(0)); //setting seed for random function in order to get different random values for attendance.
-    int dayCount = 0, monthlyWage = 0 , hoursCount = 0;
-
     while (dayCount < MAX_WORKING_DAYS && hoursCount < MAX_WORKING_HOURS)
     {
-        int dailyHours = getDailyWorkingHours();
-        hoursCount += dailyHours;
-        monthlyWage += getDailyWage(dailyHours);
+        hoursCount += generatedDailyWorkingHours();
+        monthlyWage += getDailyWage();
         dayCount++;
     }
 
     cout << "MONTHLY WAGE OF EMPLOYEE IS: " << monthlyWage << endl;
 }
 
-int getDailyWage(int hours)
+int main()
 {
-    const int WAGE_RATE = 20;
-    int dailyWage = 0;
+    cout << "WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM." << endl;
 
-    dailyWage = hours * WAGE_RATE;
-    cout << "DAILY WAGE OF EMPLOYEE IS: " << dailyWage << endl;
+    EmployeeWage employee;
+    employee.generateMonthlyWage();
 
-    return dailyWage;
-}
-
-inline int getDailyWorkingHours()
-{
-    const int FULL_TIME = 1;
-    const int PART_TIME = 2;
-    const int FULL_DAY_HOURS = 8;
-    const int PART_TIME_HOURS = 4;
-    const int ABSENT_HOURS = 0;
-
-    int attendance = getAttendance();
-
-    switch (attendance)
-    {
-    case PART_TIME:
-        cout << "EMPLOYEE IS PRESENT PART TIME AND ";
-        return PART_TIME_HOURS;
-
-    case FULL_TIME:
-        cout << "EMPLOYEE IS PRESENT FULL TIME AND ";
-        return FULL_DAY_HOURS;
-
-    default:
-        cout << "EMPLOYEE IS ABSENT AND ";
-        return ABSENT_HOURS;
-    }
-}
-
-inline int getAttendance()
-{
-    return rand() % 3;
+    return 0;
 }
