c73d761cdf97a1a397967862762ca69c714c60a6
Srijan-18 <srijan.mishrasir@gmail.com>
2020-08-27 06:10:25 +0530

    REFACTORED: Change in file structure in order to increase readability of code.

--- EmployeeWageComputation.cpp
+++ EmployeeWageComputation.cpp
@@ -1,33 +1,14 @@
 #include <iostream>
 #include <ctime>
+#include "company.cpp"
 
 using namespace std;
 
-class Company
-{
-public:
-    string companyName;
-    int MAX_WORKING_DAYS;
-    int MAX_WORKING_HOURS;
-    int WAGE_PER_HOUR;
-
-    Company(string companyName, int maxWorkingDays, int maxWorkingHours, int WagePerHour)
-    {
-        this->companyName = companyName;
-        MAX_WORKING_DAYS = maxWorkingDays;
-        MAX_WORKING_HOURS = maxWorkingHours;
-        WAGE_PER_HOUR = WagePerHour;
-    }
-};
-
 class EmployeeWageCalculator
 {
-    int MAX_WORKING_DAYS;
-    int MAX_WORKING_HOURS;
-    int WAGE_RATE;
     int dailyWage = 0, dailyHours = 0;
     int dayCount, monthlyWage, hoursCount;
-    string companyName;
+    Company company;
 
     inline int getAttendance()
     {
@@ -64,7 +45,7 @@ class EmployeeWageCalculator
 
     int getDailyWage()
     {
-        dailyWage = dailyHours * WAGE_RATE;
+        dailyWage = dailyHours * company.WAGE_PER_HOUR;
         cout << "CURRENT DAY'S WAGE OF EMPLOYEE IS: " << dailyWage << endl;
 
         return dailyWage;
@@ -80,26 +61,22 @@ void EmployeeWageCalculator::registerCompany(Company company)
     dayCount = 0;
     monthlyWage = 0;
     hoursCount = 0;
-    MAX_WORKING_DAYS = company.MAX_WORKING_DAYS;
-    MAX_WORKING_HOURS = company.MAX_WORKING_HOURS;
-    WAGE_RATE = company.WAGE_PER_HOUR;
-    companyName = company.companyName;
+    this->company = company;
 }
 
 void EmployeeWageCalculator::generateMonthlyWage()
 {
     srand(time(0)); //setting seed for random function in order to get different random values for attendance.
 
-    cout << "\n====================== " << companyName << " ======================\n"
+    cout << "\n====================== " << company.companyName << " ======================\n"
          << endl;
 
-    while (dayCount < MAX_WORKING_DAYS && hoursCount < MAX_WORKING_HOURS)
+    while (dayCount < company.MAX_WORKING_DAYS && hoursCount < company.MAX_WORKING_HOURS)
     {
         hoursCount += generatedDailyWorkingHours();
         monthlyWage += getDailyWage();
         dayCount++;
     }
-
     cout << "MONTHLY WAGE OF EMPLOYEE IS: " << monthlyWage << endl;
 }
 
@@ -108,7 +85,6 @@ int main()
     cout << "WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM." << endl;
 
     Company company1("Company1", 20, 100, 20);
-
     Company company2("Company2", 10, 50, 50);
 
     EmployeeWageCalculator employee;
---
+++ company.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+class Company
+{
+public:
+    string companyName;
+    int MAX_WORKING_DAYS;
+    int MAX_WORKING_HOURS;
+    int WAGE_PER_HOUR;
+
+    Company()
+    {
+    }
+
+    Company(string companyName, int maxWorkingDays, int maxWorkingHours, int WagePerHour)
+    {
+        this->companyName = companyName;
+        MAX_WORKING_DAYS = maxWorkingDays;
+        MAX_WORKING_HOURS = maxWorkingHours;
+        WAGE_PER_HOUR = WagePerHour;
+    }
+};
